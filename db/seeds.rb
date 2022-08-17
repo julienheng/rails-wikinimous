d07b5286823066b8e38a38511b072c1230a13632
julien heng <hengjchan@gmail.com>
2022-08-17 17:11:42 +0800

    challenge 3 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'Creating 10 fake articles..'
+
+10.times do
+  article = Article.new(
+    title: Faker::GreekPhilosophers.name,
+    content: Faker::GreekPhilosophers.quote
+  )
+  article.save
+end
+
+puts 'Finished'
